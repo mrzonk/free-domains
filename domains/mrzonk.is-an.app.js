6cb10b9aecf04f2875d694d272713b112ca184dd
galang058 <galang058@gmail.com>
2024-07-17 01:38:06 +0700

    Register mrzonk.is-an.app.js

---
+++ domains/mrzonk.is-an.app.js
@@ -0,0 +1,15 @@
+
+
+addSubDomain({
+  description: 'Demo project',
+  domain: 'is-an.app',
+  subdomain: 'mrzonk',
+  owner: {
+    repo: 'https://github.com/mrzonk/free-domains',
+    email: 'galang058@gmail.com',
+  },
+  record: {
+    A: '103.59.160.21',
+  },
+  proxy: true,
+})
